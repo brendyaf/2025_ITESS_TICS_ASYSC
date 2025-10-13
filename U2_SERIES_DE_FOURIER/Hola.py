f08da61fd796b49c845fc3af0dbafef1bcd59b6f
brendyaf <brandyaguilar001@gmail.com>
2025-10-13 14:41:25 -0600

    Modifique el script de Hola.py

---
+++ U2_SERIES_DE_FOURIER/Hola.py
@@ -0,0 +1,21 @@
+#ITESS tics
+#TI-501
+#ANALISIS DE SEÑALES Y SISTEMAS DE COMUNICACIONES
+# Profesor: Francisco Javier Montecillo Puente
+#BRANDY AGUILAR FLORES - brandyaguilar001@gmail.com
+#Este scrip imprime un Hola Mundo en Anaconda3.
+
+
+print("Hola Mundo");
+#usar python como calculadora
+
+print ("2+2");
+print(2+2);
+
+#Operaciones
+print("suma", 3+3);
+print("multiplicacion =", 6*5);
+print("division=", 7/4);
+print ("potencia", 3^4);
+print("division = ", 4//4);
+print("modulo", 4%3);
