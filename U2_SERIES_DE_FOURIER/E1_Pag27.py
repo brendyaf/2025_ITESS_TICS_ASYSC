22ce16b70ebb39d53c497f93a642ab6795fb42de
brendyaf <brandyaguilar001@gmail.com>
2025-10-13 20:51:28 -0600

    Se agrego el ejercicio E1_Pag27.py

---
+++ U2_SERIES_DE_FOURIER/E1_Pag27.py
@@ -0,0 +1,32 @@
+#ITESS tics
+#TI-501
+#ANALISIS DE SEÑALES Y SISTEMAS DE COMUNICACIONES
+# Profesor: Francisco Javier Montecillo Puente
+##BRANDY AGUILAR FLORES - brandyaguilar001@gmail.com
+# Este script muestra cómo manipular listas en Python usando algunos de sus métodos. 
+#  Se realizan operaciones para contar, insertar, agregar, buscar, eliminar, invertir,
+#  ordenar y quitar elementos, mostrando cómo cambia la lista paso a paso.
+
+
+a = [66.25, 333, 333, 1, 1234.5]
+print(a.count(333), a.count(66.25), a.count('x'))
+#2 1 0
+a.insert(2, -1)
+a.append(333)
+a
+#[66.25, 333, -1, 333, 1, 1234.5, 333]
+a.index(333)
+#1
+a.remove(333)
+a
+#[66.25, -1, 333, 1, 1234.5, 333]
+a.reverse()
+a
+#[333, 1234.5, 1, 333, -1, 66.25]
+a.sort()
+a
+#[-1, 1, 66.25, 333, 333, 1234.5]
+a.pop()
+#1234.5
+a
+#[-1, 1, 66.25, 333, 333]
