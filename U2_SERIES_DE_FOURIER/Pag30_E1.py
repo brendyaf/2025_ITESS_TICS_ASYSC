9147532139fde902b0911900009cf48da5a8c843
brendyaf <brandyaguilar001@gmail.com>
2025-10-14 06:29:46 -0600

    Actualización del ejercicio Pag30_E1.py

--- U2_SERIES_DE_FOURIER/Pag30_E1.py
+++ U2_SERIES_DE_FOURIER/Pag30_E1.py
@@ -10,14 +10,19 @@
 
 #Lista
 a = [-1, 1, 66.25, 333, 333, 1234.5]
+print("Lista: a = ", a)
+
 del a[0] # se borra el el primer numero de la lista
-a
+print("del a[0]:", a)
 #[1, 66.25, 333, 333, 1234.5]
+
 del a[2:4] # Se borran los números en esas posiciones
-a
+print("del a[2:4]:", a)
 #[1, 66.25, 1234.5]
+
 del a[:] #Se borran todos los numeros de la lista
-a
+print("del a[:]:", a)
 #[] se vacia toda la lista
+
 #del puede usarse también para eliminar variables:
 del a
