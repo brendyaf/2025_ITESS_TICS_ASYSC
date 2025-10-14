fc294ee39e89fc0c89bdee7eb758de40f50c9aae
brendyaf <brandyaguilar001@gmail.com>
2025-10-14 07:33:14 -0600

    Actualización del ejercicio Pag28_E2.py

--- U2_SERIES_DE_FOURIER/Pag28_E2.py
+++ U2_SERIES_DE_FOURIER/Pag28_E2.py
@@ -13,65 +13,87 @@
 cuadrados = []
 for x in range(10):
     cuadrados.append(x**2)
-cuadrados
+print("cuadrados = []")
+print("for x in range(10): " 
+"   cuadrados.append(x**2)")
+print(cuadrados)
 #[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-#Podemos calcular la lista de cuadrados sin ningun efecto secundario haciendo:
+# Podemos calcular la lista de cuadrados sin ningun efecto secundario haciendo:
 cuadrados = list(map(lambda x: x**2, range(10)))
+print("cuadrados = list(map(lambda x: x**2, range(10)))")
+print(cuadrados )
 
-#o un equivalente:
+# o un equivalente:
 cuadrados = [x ** 2 for x in range(10)]
+print("cuadrados = [x ** 2 for x in range(10)]")
+print(cuadrados)
 
-#Esta lista de comprensión combina los elementos de dos listas si no son iguales:
-[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+# Esta lista de comprensión combina los elementos de dos listas si no son iguales:
+print("[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]")
+resultado = [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+print(resultado) 
 #[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
 
-#Es equivalente a:
+# Es equivalente a:
 combs = []
 for x in [1, 2, 3]:
     for y in [3, 1, 4]:
         if x != y:
             combs.append((x, y))
-
-combs
+print("combs = [] "
+"for x in [1, 2, 3]: "
+"   for y in [3, 1, 4]:"
+"       if x != y:" 
+"           combs.append((x, y))")
+print(combs)
 #[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
 
-# crear una nueva lista con los valores duplicados
+# Crear una nueva lista con los valores duplicados
 vec = [-4, -2, 0, 2, 4]
-[x * 2 for x in vec]
+print("vec = [-4, -2, 0, 2, 4]")
+print("[x*2 for x in vec]")
+print([x * 2 for x in vec])
 #[-8, -4, 0, 4, 8]
 
-# filtrar la lista para excluir números negativos
-[x for x in vec if x >= 0]
+# Filtrar la lista para excluir números negativos
+print("[x for x in vec if x >= 0]")
+print([x for x in vec if x >= 0])
 #[0, 2, 4]
 
-# aplica una función a todos los elementos
-[abs(x) for x in vec]
+# Aplica una función a todos los elementos
+print("[abs(x) for x in vec]")
+print([abs(x) for x in vec])
 #[4, 2, 0, 2, 4]
 
-# llama un método a cada elemento
+# Llama un método a cada elemento
+print("frutafresca = [' banana', ' mora de Logan ', 'maracuya ']")
 frutafresca = [' banana', ' mora de Logan ', 'maracuya ']
-[arma.strip() for arma in frutafresca]
-['banana', 'mora de Logan', 'maracuya']
+print([arma.strip() for arma in frutafresca])
+#['banana', 'mora de Logan', 'maracuya']
 
-# crea una lista de tuplas de dos como (número, cuadrado)
-[(x, x ** 2) for x in range(6)]
+# Crea una lista de tuplas de dos como (número, cuadrado)
+print("[(x, x ** 2) for x in range(6)]")
+print([(x, x ** 2) for x in range(6)])
 #[(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
 # la tupla debe estar entre paréntesis, sino es un error
 
 # Ejemplo de error de sintaxis: olvida los paréntesis en la tupla
+# print("[x, x ** 2 for x in range(6)]")
 # [x, x ** 2 for x in range(6)]
 # Esto produce:
 # SyntaxError: invalid syntax
 
-# aplanar una lista usando comprensión de listas con dos 'for'
+# Aplanar una lista usando comprensión de listas con dos 'for'
+print("vec = [[1,2,3], [4,5,6], [7,8,9]]")
 vec = [[1,2,3], [4,5,6], [7,8,9]]
-[num for elem in vec for num in elem]
+print([num for elem in vec for num in elem])
 #[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-#Las comprensiones de listas pueden contener expresiones complejas y funciones anidadas:
+# Las comprensiones de listas pueden contener expresiones complejas y funciones anidadas:
 from math import pi
-[str(round(pi, i)) for i in range(1, 6)]
+print("[str(round(pi, i)) for i in range(1, 6)]")
+print([str(round(pi, i)) for i in range(1, 6)])
 #['3.1', '3.14', '3.142', '3.1416', '3.14159']
 
 
